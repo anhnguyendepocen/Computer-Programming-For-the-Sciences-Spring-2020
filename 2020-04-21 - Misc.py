1ccdcbe6066985884c22c57748e39e26c7ab9771
Brian Blais <bblais@gmail.com>
2020-04-21 19:45:30 -0400

    example with interpolating data

--- 2020-04-21 - Misc.py
+++ 2020-04-21 - Misc.py
@@ -38,6 +38,64 @@ polyfit(t-1950,T,1)
 polyfit(t,T,1)
 
 
+# In[3]:
+
+
+from pyndamics import *
+
+
+# In[5]:
+
+
+t=linspace(0,10,100)
+S=sin(t*3)
+plot(t,S)
+
+
+# In[17]:
+
+
+class InterpData(object):
+    
+    def __init__(self,x,y,name):
+        self.x=x
+        self.y=y
+        self.__name__=name
+        
+    def __call__(self,x):
+        from numpy import interp
+        y=interp(x,self.x,self.y)
+        return y
+
+t=linspace(0,10,100)  # made up data, you can use the real thing
+S=sin(t*3)
+
+F=InterpData(t,S,'F')
+
+
+# In[18]:
+
+
+sim=Simulation()
+sim.add("v'=F(t)/m",0,plot=True)
+sim.add("x'=v",0,plot=True)
+sim.params(m=1)
+sim.functions(F)
+sim.run(0,10)
+
+
+# In[10]:
+
+
+dir(sim)
+
+
+# In[11]:
+
+
+get_ipython().run_line_magic('pinfo', 'sim.functions')
+
+
 # In[ ]:
 
 
